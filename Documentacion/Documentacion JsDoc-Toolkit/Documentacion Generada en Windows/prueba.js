2a2815b5b89a44f13410144e2d0d91067dafd1c7
andrea ochoa <alom5934@gmail.com>
2016-04-12 15:59:26 -0500

    Documentacion del repositorio.

---
+++ Documentacion/Documentacion JsDoc-Toolkit/Documentacion Generada en Windows/prueba.js
@@ -0,0 +1,9 @@
+/** @description Determines the area of a circle that has the specified radius parameter.
+ * @param {number} radius The radius of the circle.
+ * @return {number}
+ */
+function getArea(radius) {
+    var areaVal;
+    areaVal = Math.PI * radius * radius;
+    return areaVal;
+}
