80b88be32439a61793008e2bf7486dda668414e8
Cristina Cespedes <cristinacespedes01@gmail.com>
2016-04-19 15:30:48 -0500

    Update Cantidad_Enviados.js
    Se eliminaron algunos métodos que eran utilizados para la autenticación y se encontraban repetidos en otros códigos, los cuales se dejaran solamente en un código.

--- Version_2.0/Sources/Scripts/Cantidad_Enviados.js
+++ Version_2.0/Sources/Scripts/Cantidad_Enviados.js
@@ -1,53 +1,6 @@
 /**
 * @author Cris
 */
-// ID del cliente para acceder a una aplicación web usando la consola de desarrollo de gmail.
-var CLIENT_ID = '809466617807-82ulq5gstd6l2bfrndutji0to1ink7ba.apps.googleusercontent.com';
-// Permisos del usuario.
-var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-/**
-* Comprueba que el usuario aceptara la solicitud.
-*/			
-function checkAuth() {
-	gapi.auth.authorize({
-		'client_id': CLIENT_ID,
-		'scope': SCOPES.join(' '),
-		'immediate': true
-	},
-	handleAuthResult);
-}
-/**
-* Maneja la respuesta de autorización del servidor.
-* @param {Object} authResult resultado de la autorización.
-*/
-function handleAuthResult(authResult) {
-	var authorizeDiv = document.getElementById('authorize-div');
-	if (authResult && !authResult.error) {
-		// Hide auth UI, then load client library.
-		authorizeDiv.style.display = 'none';
-        loadGmailApi();
-	} else {
-		// Show auth UI, allowing the user to initiate authorization by
-		// clicking authorize button.
-		authorizeDiv.style.display = 'inline';
-	}
-}			
-function handleAuthClick(event) {
-	gapi.auth.authorize({
-		client_id: CLIENT_ID,
-		scope: SCOPES, 
-		immediate: false
-	},
-	handleAuthResult);
-return false;
-}
-/**
-* Cargar la biblioteca de cliente de la API de Gmail . 
-* lista la cantidad de mensajes una vez se carga la biblioteca de cliente .
-*/
-function loadGmailApi() {
-	gapi.client.load('gmail', 'v1',cantidadEnviados);
-}
 /**
 * Obtiene la información de la cantidad de mensajes enviados por el usuario autorizado.
 */						
@@ -62,12 +15,4 @@ function cantidadEnviados(){
 			cantidad = messages.length;					
 			appendPre('Cantidad De Mensajes enviados: '+cantidad);	
 	});	
-}
-/**
-* Imprime en pantalla.
-*/	 
-function appendPre(message) {
-	var pre = document.getElementById('output');
-	var textContent = document.createTextNode(message + '\n');
-	pre.appendChild(textContent);
 }
