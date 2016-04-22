7b765ccc5cbe78b4adffc1f2edf7eabdfbea7f9e
Cristina Cespedes <cristinacespedes01@gmail.com>
2016-04-21 19:00:10 -0500

    Extracción de fechas
    Con este código podemos extraer la fecha en la que fue enviado el mensaje

---
+++ Version_2.0/Examples/Scripts/Fechas.js
@@ -0,0 +1,17 @@
+function fechas() {
+        var request = gapi.client.gmail.users.messages.list({
+          'userId': 'me',
+          'labelIds': 'SENT'          
+        });
+        request.execute(function(response) {
+			$.each(response.messages, function() {
+				var messageRequest = gapi.client.gmail.users.messages.get({
+					'userId': 'me',
+					'id': this.id
+				});
+				messageRequest.execute(function extraer(message) {			
+					appendPre(encabezado(message.payload.headers, 'Date'));			
+				});
+			});
+        });
+    }
